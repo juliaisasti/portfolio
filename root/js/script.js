57be9989c5520f654a9861a415716dfa85810ff8
Julia <juliaisasti@gmail.com>
2023-10-02 17:34:23 +0200

    blabla

--- root/js/script.js
+++ root/js/script.js
@@ -1,1 +1,18 @@
-console.log('Welcome to Fullstack');
+document.getElementById("boton1").addEventListener("click", function () {
+  let ul = document.createElement("ul");
+  let li = document.createElement("li");
+  let elements = document.createTextNode("texto dentro de li");
+  let li2 = document.createElement("li");
+  let elements2 = document.createTextNode("texto dentro de li2");
+  let li3 = document.createElement("li");
+  let elements3 = document.createTextNode("texto dentro de li3");
+
+  ul.appendChild(li);
+  ul.appendChild(li2);
+  ul.appendChild(li3);
+  li.appendChild(elements);
+  li2.appendChild(elements2);
+  li3.appendChild(elements3);
+
+  document.querySelector("#lista").appendChild(ul);
+});
